4fe876e31c499f5faa66fb4270b35f8dfb88d7bf
pwndr3 <andre.vallieres@mail.mcgill.ca>
2020-01-25 22:19:51 -0500

    checkIfLegal done

--- assignment1/functions.cpp
+++ assignment1/functions.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <string>
 
+#define NUMBER_OF_ROWS (3)
+#define NUMBER_OF_COLS (9)
+#define NUMBER_OF_CELLS (NUMBER_OF_ROWS * NUMBER_OF_COLS)
+
 using namespace std;
+
+/*
+TODO:
+    Validate input parameters
+*/
 
 void greetAndInstruct() {
     // Greeting message
@@ -27,11 +36,11 @@ void greetAndInstruct() {
 }
 
 void displayBoard (char board[]) {
-    string boardASCII[3][9]; // Matrix that will contain the ASCII board
+    string boardASCII[NUMBER_OF_ROWS][NUMBER_OF_COLS]; // Matrix that will contain the ASCII board
 
-    for(int row = 0; row < 3; row++) {
-        for(int col = 0; col < 9; col++) {
-            int idx = (col / 3) * 9 + (col % 3) + (row * 3) + 1;
+    for(int row = 0; row < NUMBER_OF_ROWS; row++) {
+        for(int col = 0; col < NUMBER_OF_COLS; col++) {
+            int idx = (col / NUMBER_OF_ROWS) * NUMBER_OF_COLS + (col % NUMBER_OF_ROWS) + (row * NUMBER_OF_ROWS) + 1;
 
             if(board[idx] == 0)
                 boardASCII[row][col] = to_string(idx); // Empty cell
@@ -40,9 +49,41 @@ void displayBoard (char board[]) {
         }
     }
 
-    for(int row = 0; row < 3; row++) {
+    for(int row = 0; row < NUMBER_OF_ROWS; row++) {
         cout << boardASCII[row][0] << " | " << boardASCII[row][1] << " | " << boardASCII[row][2] << "          ";
         cout << boardASCII[row][3] << " | " << boardASCII[row][4] << " | " << boardASCII[row][5] << "          ";
         cout << boardASCII[row][6] << " | " << boardASCII[row][7] << " | " << boardASCII[row][8] << endl;
     }
+}
+
+bool checkIfLegal(int cellNbre, char board[]) {
+    // Verify cellNbre input range
+    if(cellNbre < 1 || cellNbre > NUMBER_OF_CELLS)
+        return false; 
+
+    // Check if already occupied
+    if(board[cellNbre - 1] != 0)
+        return false;
+
+    return true;
+}
+
+bool checkWinner(char board[]) {
+    // Check row in same table
+
+
+    // Check row across tables
+
+    
+    // Check column in same table
+
+
+    // Check column across tables
+
+
+    // Check diagonal in same table
+
+
+    // Check diagonal across tables
+    
 }
--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -3,14 +3,13 @@
 /* Functions from functions.cpp */
 void greetAndInstruct();
 void displayBoard (char board[]);
+bool checkIfLegal(int cellNbre, char board[]);
 /**/
 
 int main(int argc, char **argv) {
     //greetAndInstruct();
     
     char board[27] = {0};
-    board[8] = 'X';
-    board[20] = 'O';
     displayBoard(board);
 
     return 0;
