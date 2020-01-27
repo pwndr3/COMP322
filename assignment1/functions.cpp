4f453e5c45eb7cfceaf7afe5160e0a521662a380
pwndr3 <andre.vallieres@mail.mcgill.ca>
2020-01-27 03:56:43 -0500

    Almost done, but some TODOS
    Further tests required, not working now
    Need better AI than random

--- assignment1/functions.cpp
+++ assignment1/functions.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 #include <string>
 
-#define NUMBER_OF_ROWS (3)
-#define NUMBER_OF_COLS (9)
-#define NUMBER_OF_CELLS (NUMBER_OF_ROWS * NUMBER_OF_COLS)
-
 using namespace std;
 
 /*
 TODO:
     Validate input parameters
+    Maybe C++ syntax for loops is better?
 */
 
 void greetAndInstruct() {
@@ -35,21 +32,22 @@ void greetAndInstruct() {
     exit(0);
 }
 
+// TODO make sure this works correctly
 void displayBoard (char board[]) {
-    string boardASCII[NUMBER_OF_ROWS][NUMBER_OF_COLS]; // Matrix that will contain the ASCII board
+    string boardASCII[3][9]; // Matrix that will contain the ASCII board
 
-    for(int row = 0; row < NUMBER_OF_ROWS; row++) {
-        for(int col = 0; col < NUMBER_OF_COLS; col++) {
-            int idx = (col / NUMBER_OF_ROWS) * NUMBER_OF_COLS + (col % NUMBER_OF_ROWS) + (row * NUMBER_OF_ROWS) + 1;
+    for(int row = 0; row < 3; row++) {
+        for(int col = 0; col < 9; col++) {
+            int idx = (col / 3) * 9 + (col % 3) + (row * 3) + 1;
 
-            if(board[idx] == 0)
+            if(board[idx - 1] == 0)
                 boardASCII[row][col] = to_string(idx); // Empty cell
             else
-                boardASCII[row][col] = board[idx];
+                boardASCII[row][col] = board[idx - 1];
         }
     }
 
-    for(int row = 0; row < NUMBER_OF_ROWS; row++) {
+    for(int row = 0; row < 3; row++) {
         cout << boardASCII[row][0] << " | " << boardASCII[row][1] << " | " << boardASCII[row][2] << "          ";
         cout << boardASCII[row][3] << " | " << boardASCII[row][4] << " | " << boardASCII[row][5] << "          ";
         cout << boardASCII[row][6] << " | " << boardASCII[row][7] << " | " << boardASCII[row][8] << endl;
@@ -58,7 +56,7 @@ void displayBoard (char board[]) {
 
 bool checkIfLegal(int cellNbre, char board[]) {
     // Verify cellNbre input range
-    if(cellNbre < 1 || cellNbre > NUMBER_OF_CELLS)
+    if(cellNbre < 1 || cellNbre > 27)
         return false; 
 
     // Check if already occupied
@@ -69,21 +67,127 @@ bool checkIfLegal(int cellNbre, char board[]) {
 }
 
 bool checkWinner(char board[]) {
-    // Check row in same table
+    char winnerSymbol = 0;
 
+    // Check row in same table
+    if(winnerSymbol == 0) {
+        for(int table = 0; table < 3; table++) {
+            for(int row = 0; row < 3; row++) {
+                int row_idx = table * 9 + row * 3;
+
+                // If row has three same symbol, we have a winner
+                if(board[row_idx] == board[row_idx + 1] && board[row_idx] == board[row_idx + 2])
+                    winnerSymbol = board[row_idx];
+            }
+        }
+    }
 
     // Check row across tables
-
+    if(winnerSymbol == 0) {
+        for(int row = 0; row < 3; row++) {
+            // x|.|.   .|x|.   .|.|x
+            if(board[3*row] == board[3*row + 10] && board[3*row] == board[3*row + 20])
+                winnerSymbol = board[3*row];
+            // .|.|x   .|x|.   x|.|.
+            else if(board[3*row + 2] == board[3*row + 10] && board[3*row] == board[3*row + 18])
+                winnerSymbol = board[3*row + 2];
+        }
+    }
     
     // Check column in same table
+    if(winnerSymbol == 0) {
+        for(int table = 0; table < 3; table++) {
+            for(int col = 0; col < 3; col++) {
+                int col_idx = table * 9 + col;
 
+                // If col has three same symbol, we have a winner
+                if(board[col_idx] == board[col_idx + 3] && board[col_idx] == board[col_idx + 6])
+                    winnerSymbol = board[col_idx];
+            }
+        }
+    }
 
     // Check column across tables
-
+    if(winnerSymbol == 0) {
+        for(int col = 0; col < 3; col++) {
+            // x|.|.   
+            //           x|.|.
+            //                       x|.|.
+            if(board[col] == board[col + 12] && board[col] == board[col + 24])
+                winnerSymbol = board[col];
+            //                       x|.|. 
+            //           x|.|.
+            // x|.|. 
+            else if(board[col+6] == board[col + 12] && board[col+6] == board[col + 18])
+                winnerSymbol = board[col+6];
+        }
+    }
 
     // Check diagonal in same table
-
+    if(winnerSymbol == 0) {
+        for(int table = 0; table < 3; table++) {
+            // x|.|.
+            // .|x|. 
+            // .|.|x
+            if(board[table * 9] == board[table * 9 + 4] && board[table * 9] == board[table * 9 + 8])
+                winnerSymbol = board[table * 9];
+            // .|.|x
+            // .|x|. 
+            // x|.|.
+            else if(board[table * 9 + 2] == board[table * 9 + 4] && board[table * 9 + 2] == board[table * 9 + 6])
+                winnerSymbol = board[table * 9 + 2];
+        }
+    }
 
     // Check diagonal across tables
-    
+    if(winnerSymbol == 0) {
+        // x|.|.   
+        //           .|x|.
+        //                       .|.|x
+        if(board[0] == board[13] && board[0] == board[26])
+            winnerSymbol = board[0];
+        // .|.|x   
+        //           .|x|.
+        //                       x|.|.
+        else if(board[2] == board[13] && board[2] == board[24])
+            winnerSymbol = board[2];
+        //                       x|.|. 
+        //           .|x|.
+        // .|.|x 
+        else if(board[8] == board[13] && board[8] == board[18])
+            winnerSymbol = board[8]; 
+        //                       .|.|x 
+        //           .|x|.
+        // x|.|. 
+        else if(board[6] == board[13] && board[6] == board[20])
+            winnerSymbol = board[6];     
+    }
+
+    // Check same space across tables
+    if(winnerSymbol == 0) {
+        for(int cell = 0; cell < 9; cell++) {
+            if(board[cell] == board[cell + 9] && board[cell] == board[cell + 18])
+                winnerSymbol = board[cell];
+        }
+    }
+
+    if(winnerSymbol != 0) {
+        if(winnerSymbol == 'X')
+            cout << "END! Player has won the game!" << endl; 
+        else
+            cout << "END! Computer has won the game!" << endl;
+
+        return true;
+    }
+
+    return false;
+}
+
+void computerMove(char board[]) {
+    for(int i = 1; i <= 27; i++) {
+        if(checkIfLegal(i, board)) {
+            board[i-1] = 'O';
+            break;
+        }
+    }
 }
--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -1,16 +1,46 @@
 #include <iostream>
 
+using namespace std;
+
 /* Functions from functions.cpp */
 void greetAndInstruct();
 void displayBoard (char board[]);
 bool checkIfLegal(int cellNbre, char board[]);
+bool checkWinner(char board[]);
+void computerMove(char board[]);
 /**/
 
 int main(int argc, char **argv) {
-    //greetAndInstruct();
+    greetAndInstruct();
     
     char board[27] = {0};
-    displayBoard(board);
+
+    int turnNumber = 1;
+    while(!checkWinner(board) && turnNumber++ <= 27) {
+        // Display board for player
+        displayBoard(board);
+
+        // Player move
+        string playerMove;
+        cout << "Pick your move: ";
+        cin >> playerMove;
+
+        while(!checkIfLegal(stoi(playerMove), board)) {
+            cout << "Invalid move: " << playerMove << endl;
+            cout << "Pick your move: ";
+            cin >> playerMove;
+        } 
+        board[stoi(playerMove) - 1] = 'X';
+
+        if(checkWinner(board))
+            break; // If player won the game, break now
+
+        // Computer move
+        computerMove(board);
+    } 
+
+    if(turnNumber == 27)
+        cout << "END! It's a TIE!" << endl;
 
     return 0;
 }
